46e5e704f217c7338aff810bf949948a3447a33a
pensarguet <philippe.ensarguet@gmail.com>
2021-06-10 22:10:18 +0200

    Update images at Thu 10 Jun 22:10:18 CEST 2021
    Signed-off-by: pensarguet <philippe.ensarguet@gmail.com>

--- app/sample.sh
+++ app/sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3 app.py
 
-export GIT_SSH_COMMAND="ssh -i `pwd`/.ssh/id_rsa"
+export GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa"
 
 cp ${HOME}/growlab/app/html/* ${HOME}/growlab/docs/
 
