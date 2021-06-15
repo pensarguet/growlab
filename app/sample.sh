8a1162b96b8f8f7a0161f856bfe0fc2daa2cde79
pensarguet <philippe.ensarguet@gmail.com>
2021-06-15 16:26:28 +0200

    Update sample.sh to adapt to local implementation

--- app/sample.sh
+++ app/sample.sh
@@ -6,9 +6,10 @@ export GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa"
 
 cp ${HOME}/growlab/app/html/* ${HOME}/growlab/docs/
 
-git add .
+git add ../docs/index.html
+git add ../docs/preview.jpg
 
 git commit -s -m "Update images at `date`"
-git pull origin master --rebase
+# git pull origin master --rebase
 git push origin master
 
